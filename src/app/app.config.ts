be683fd8835ea5851c42f705afcdacc0d3d05e24
Davide Di Francesco <davdifr@outlook.com>
2024-07-30 20:22:24 +0200

    register and backendErrors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,17 +6,20 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideHttpClient } from '@angular/common/http';
+import { authFeatureKey, authReducer } from './auth/store/reducers';
+import * as authEffects from './auth/store/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideStore(),
-    provideEffects(),
+    provideState(authFeatureKey, authReducer),
+    provideEffects(authEffects),
     provideHttpClient(),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { authActions } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { CommonModule } from '@angular/common';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
+import { combineLatest } from 'rxjs';
+import { BackendErrorMessagesComponent } from '../../../shared/components/backendErrorMessages/backendErrorMessages.component';
 
 @Component({
   selector: 'cce-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, BackendErrorMessagesComponent],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  #fb = inject(FormBuilder);
+  #store = inject(Store);
+
+  form = this.#fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  data$ = combineLatest({
+    isSubmiting: this.#store.select(selectIsSubmitting),
+    backendErrors: this.#store.select(selectValidationErrors),
+  });
+
+  constructor() {}
+
+  onSubmit() {
+    console.log(this.form.getRawValue());
+
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+
+    this.#store.dispatch(authActions.register({ request }));
+  }
+}
